b0bb035e023f54a55824f2e2c6ff6f6a8c0d8787
Torsten Seemann <torsten.seemann@gmail.com>
2017-08-09 15:57:01 +1000

    bug fix for path versus exe

--- bin/hash_path.pl
+++ bin/hash_path.pl
@@ -4,17 +4,19 @@ use warnings;
 use Data::Dumper;
 use File::Spec;
 
-my %seen;
+my %seen_path;
+my %seen_exe;
 my $count;
 
 for my $p ( File::Spec->path() ) {
-  next if $seen{$p}++;
+  next if $seen_path{$p}++;
 #  print STDERR "Parsing: $p\n";
   opendir(my $dh, $p) or next;
   while (readdir $dh) {
     next if m/^\./;
     next if m/~$/;
 #    print STDERR "# $p/$_\n";
+    next if $seen_exe{$_}++;
     if (-r "$p/$_" && !-d _ && -x _) {
       print "hash -p '$p/$_' '$_'\n";  
       $count++;
