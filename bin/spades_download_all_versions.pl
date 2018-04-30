4b8df4f08a3d9847c156fa0dc16f30802e852ac3
Torsten Seemann <torsten.seemann@gmail.com>
2018-04-30 11:46:47 +1000

    Skip bad version; make list; ARCH support

--- bin/spades_download_all_versions.pl
+++ bin/spades_download_all_versions.pl
@@ -7,26 +7,49 @@ use LWP::Simple;
 #   mkdir spades
 #   cd spades
 #   spades_download_all_versions.pl > Makefile
-#   make        # download all one by one
-#   make -j     # download all in parallel
-#   make 3.8.1  # download a specific version
+#
+#   make list   # see versions
+#
+#   make              # download all one by one
+#   make ARCH=Darwin  # download all one by one
+#   make -j           # download all in parallel
+#   make 3.8.1        # download a specific version
+#
 #   3.8.1/bin/spades.py --help
-
+#
+#   make clean  # remove all .tgz files
+#
 
 my $ARCH = "Linux";
 my $ROOT = "http://cab.spbu.ru/files";
 
+my %seen;
 my %ver;
 print STDERR "Reading: $ROOT\n";
 my $index = get($ROOT) or die "Can't read index: $ROOT";
 while ($index =~ m/(release(\d+\.\d+.\d+))/g) {
-  $ver{$2} = [ "$ROOT/$1", "SPAdes-$2-$ARCH.tar.gz" ];
+  my($rel,$v) = ($1,$2);
+  next if $seen{$v}++;
+  if ($v !~ m/^2\.[123]/) {
+    $ver{$v} = [ "$ROOT/$rel", "SPAdes-$v-\$(ARCH).tar.gz" ];
+  }
+  else {
+    print STDERR "Skipping $rel which has no binaries.\n";
+  }
 }
 
 print STDERR "Making Makefile\n";
+
 my @ver = reverse sort { $a <=> $b } keys %ver;
-print ".DELETE_ON_ERROR:\n\n";
+my @clean;
+
+print "ARCH=Linux # use Darwin for macOS\n\n";
+
+print ".DELETE_ON_ERROR:\n";
+print ".PHONY: all list clean\n\n";
 print "all: @ver\n\n";
+print "list:\n\t\@echo @ver | tr ' ' '\\n'\n\n";
+
 for my $ver (reverse sort { $a <=> $b } keys %ver) {
   my($url,$fname) = @{$ver{$ver}};
   print "$ver: $fname\n";
@@ -34,5 +57,8 @@ for my $ver (reverse sort { $a <=> $b } keys %ver) {
   print "\ttar --strip-components 1 -C \$\@ -x -f '$fname'\n\n";
   print "$fname:\n";
   print "\twget $url/$fname\n\n";
+  push @clean, $fname;
 }
+
+print "clean:\n\t\$(RM) @clean\n\n";
 print STDERR "Done.\n";
